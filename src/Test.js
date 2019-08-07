9c672982733fac15ca7b0a432b833345978ab8cf
jews3333 <jews333333@gmail.com>
2019-08-07 17:57:21 +0900

    '로그인작업중'

--- src/Test.js
+++ src/Test.js
@@ -10,34 +10,69 @@ class Test extends React.Component {
         super();
         this.state = {
             list: {},
-            user: this.login
+            user: false,
+            token: false
         }
         this.login = this.login.bind(this);
         this.logout = this.logout.bind(this);
     }
 
     async login() {
-        const result = await auth().signInWithPopup(provider);
-        console.log(result.user.uid);
-        this.setState({
-            user: result.user
-        });
-    }
-    
-    async loginCheck(){
-        const ck = await auth().currentUser;
-        console.log(ck);
+        await auth().signInWithRedirect(provider)
+        .then((result) => {
+            const user = result.user;
+            const token = result.credential.accessToken;
+            
+            this.setState({
+                user: user
+            });
+            console.log(user);
+            console.log(token);
+        })
+        .catch((err) => {
+            console.log(err.code);
+            console.log(err.message);
+        })
     }
 
     async logout() {
         await auth().signOut();
         this.setState({
-            user: false
+            user: false,
+            token: false
+        });
+    }
+
+    async loginCeck() {
+        await auth().getRedirectResult()
+        .then(result => {
+            this.setState({
+                token : result.credential.accessToken
+            })
+            console.log(this.state.token);
+        })
+        .catch((err) => {
+            console.log(err.code);
+            console.log(err.message);
+        })
+    }
+
+    toast = () => {
+        const toast = document.createElement("div");
+        const text = document.createTextNode("권한이 없습니다");
+        toast.appendChild(text);
+        toast.className = "toast";
+        document.body.appendChild(toast);
+        setTimeout(()=>{
+            toast.classList.add("load");
+            setTimeout(()=>{
+                toast.classList.remove("load");
+            },2000);
         });
     }
 
     _getApi = () => {
-        if(this.state.user.uid === masterUID){
+        if(this.state.token || this.state.user.uid === masterUID){
             fetch(`${databaseURL}/list.json`)
             .then(res => {
                 if(res.status !== 200){
@@ -46,28 +81,19 @@ class Test extends React.Component {
 
                 return res.json();
             })
-            .then(list => this.setState({list : list}))
+            .then(list => this.setState({list : list}));
         } else {
-            const toast = document.createElement("div");
-            const text = document.createTextNode("권한이 없습니다");
-            toast.appendChild(text);
-            toast.className = "toast";
-            document.body.appendChild(toast);
-            setTimeout(()=>{
-                toast.classList.add("load");
-                setTimeout(()=>{
-                    toast.classList.remove("load");
-                },2000);
-            });
+            this.toast();
         }
     }
 
-    shouldComponentUpdate(nextProps, nextState){
-        return nextState !== this.state;
-    }
-    
+    // shouldComponentUpdate(nextProps, nextState){
+        
+    //     return nextState !== this.state;
+    // }
+
     componentDidMount(){
-        this.loginCheck();
+        this.loginCeck();
     }
 
     render(){
@@ -75,8 +101,8 @@ class Test extends React.Component {
 
         return(
             <div>
-                {!this.state.user.uid ? <button onClick={this.login}>로그인 테스트</button> : <button onClick={this.logout}>로그아웃 테스트</button>}
-                
+                {!this.state.token ? <button onClick={this.login}>로그인 테스트</button> : <button onClick={this.logout}>로그아웃 테스트</button>}
+
                 {Object.keys(this.state.list).map(list => {
                     const item = this.state.list[list];
                     return (
